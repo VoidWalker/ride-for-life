e0a25e547ad052f5846589c93ae1508cf4837731
a.sohan <a.sohan@ism-ukraine.com>
2016-02-17 17:16:13 +0200

    HTML moved to template

--- productlist.php
+++ productlist.php
@@ -3,16 +3,6 @@ require_once "DB.php";
 
 $db = DB::DBInstance()->DB();
 $stmt = $db->query("SELECT * FROM product");
-var_dump($stmt);
 $products = $stmt->fetchAll();
-echo "<table>";
-foreach ($products as $product) {
-    echo "<tr>";
-    echo "<td>" . $product->id . "</td>";
-    echo "<td>" . $product->name . "</td>";
-    echo "<td>" . $product->description . "</td>";
-    echo "<td>" . $product->price . "</td>";
-    echo "<td>" . $product->image . "</td>";
-    echo "</tr>";
-}
-echo "</table>";
+
+include "templates/productList.phtml";

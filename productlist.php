47135641397143af7aefc824bbc1c6da888af533
VoidWalker <sohanoleksa@gmail.com>
2016-02-17 23:22:38 +0200

    On the way to MVC

--- productlist.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "DB.php";
-
-$db = DB::DBInstance()->DB();
-$stmt = $db->query("SELECT * FROM product");
-$products = $stmt->fetchAll();
-
-include "templates/productList.phtml";
